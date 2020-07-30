761ab1c7488698f0666d924763fbec2eded150a0
Pavaloz99 <avalozp@gmail.com>
2020-07-29 18:50:52 -0700

    config variable changes

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const connectionString =
 
 
 
-mongoose.connect(process.env.MONGOLAB_WHITE_URI || connectionString, {
+mongoose.connect(process.env.MONGODB_URI || connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
